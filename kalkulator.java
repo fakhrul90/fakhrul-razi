0d9f9f7f5b476c79bb65d7850fcc89352babb255
fakhrul90 <fakhrulrazi892@gmail.com>
2021-04-20 13:04:23 +0700

    main

--- kalkulator.java
+++ kalkulator.java
@@ -17,13 +17,5 @@ public class kalkulator
      int pembagian = x / y;
         
     
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     
-    public int add(){
-        return x + y;
-    }*/
+    
 }
