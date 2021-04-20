9884e07e7643ca65ff043cca0f1f330e6b190764
fakhrul90 <fakhrulrazi892@gmail.com>
2021-04-20 12:49:16 +0700

    Main

--- Main.java
+++ Main.java
@@ -1,24 +1,22 @@
-
 /**
- * class Main 
+ *class Main 
  *
  * @author Fakhrul Razi
  * @version 0.1
  */
 public class Main{
-     
-    public static void main (string args[]){
-        //kalkulator kalkul = new kalkulator();
-        
-        kalkul.name = "operator ";
-        
-        system.out.println("nilai 1 = "+kalkul.x);
-        system.out.println("nilai 1 = "+kalkul.y);
-        
-        system.out.println(kalkul.name + "penjumlahan 4 + 2 = "+ kalkul.penjumlahan);
-        system.out.println(kalkul.name + "pengurangan 4 - 2 = "+ kalkul.pengurangan);
-        system.out.println(kalkul.name + "perkalian 4 * 2 = "+ kalkul.perkalian);
-        system.out.println(kalkul.name + "pembagian 4 / 2 = "+ kalkul.pembagian);
+   
+        public static void main (String args[]){
+            kalkulator kalkul = new kalkulator();
+            
+            kalkul.name = "operator ";
+            
+            System.out.println("nilai 1 = "+ kalkul.x);
+            System.out.println("nilai 1 = "+ kalkul.y);
+            
+            System.out.println(kalkul.name + "penjumlahan 4 + 2 = "+ kalkul.penjumlahan);
+            System.out.println(kalkul.name + "pengurangan 4 - 2 = "+ kalkul.pengurangan);
+            System.out.println(kalkul.name + "perkalian 4 * 2 = "+ kalkul.perkalian);
+            System.out.println(kalkul.name + "pembagian 4 / 2 = "+ kalkul.pembagian);
+        }
     }
-}
-        
