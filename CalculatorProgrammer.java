b33ec099bcb8c14383d39d5f1241dcafc49af96a
fakhrul90 <fakhrulrazi892@gmail.com>
2021-04-22 10:24:19 +0700

    CalculatorProgrammer

--- CalculatorProgrammer.java
+++ CalculatorProgrammer.java
@@ -1,33 +1,124 @@
-
 /**
- * Write a description of class CalculatorProgrammer here.
+ * class CalculatorProgrammer 
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author dientaufan
+ * @version 0.1
  */
-public class CalculatorProgrammer
+public class CalculatorProgrammer extends Calculator
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    public int op1=0;
+    public double op2=0;
+    public static final String info = "Kalkulatorku";
+    public static int jumlahobject = 0;
 
     /**
-     * Constructor for objects of class CalculatorProgrammer
+     * Constructor for objects of class Calculator
      */
     public CalculatorProgrammer()
     {
         // initialise instance variables
-        x = 0;
+        this.jumlahobject += 1;
     }
-
+    
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+     * Constructor for objects of class Calculator
+     * @param  double, double inisialisasi nilai dari op1 dan op2
+    */
+  
+    public CalculatorProgrammer(int op1, double op2)
     {
-        // put your code here
-        return x + y;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.jumlahobject += 1;
+    
     }
+    
+    /**
+     * Constructor for objects of class Calculator
+     * @param  double, double inisialisasi nilai dari op1 dan op2
+    */
+  
+    public CalculatorProgrammer(double op2)
+    {
+        this.op2 = op2;
+        this.jumlahobject += 1;
+        
+    
+    }
+    
+    
+    
+    
+    /**
+     * set op1
+     *
+     * @param  double op1  
+     * @return  void
+     */
+    public void setOp1(int op1)
+    {
+        this.op1 = op1;
+    }
+    
+    
+    /**
+     * get op1
+     *
+     * @param  double op1  
+     * @return  void
+     */
+    public double  getOp1()
+    {
+        return this.op1;
+    }
+    
+    
+    /**
+     * jumlah tanpa parameter
+     *
+     * @param  
+     * @return  double hasil penjumlahan op1 + op2
+     */
+    public double jumlah() 
+    {
+        return this.op1 + this.op2;
+    }
+    
+     /**
+     * jumlah dengan parameter
+     *
+     * @param  double op1
+     *         double op2
+     * @return double hasil penjumlahan op1 + op2
+     */
+    public double jumlah(int op1, double op2) 
+    {
+        this.op1 = op1;
+        this.op2 = op2;
+        return jumlah();
+    }
+    
+    /**
+     * konversiBilanganDesimaltoHex
+     *
+     * @param  double op1
+     *         double op2
+     * @return double hasil penjumlahan op1 + op2
+     */
+    public String konversiBilanganDesimaltoHex(int op1) 
+    {
+        // buat konversi ke hex dengan java (tugas)
+        String hasilKonversi = Integer.toHexString(op1) ;
+        return hasilKonversi;
+    }
+    
+    // method konversiBilanganDesimaltoBiner
+    public String konversiBilanganDesimaltoBinner(int op1) 
+    {
+        // buat konversi ke hex dengan java (tugas)
+        String hasilKonversi = Integer.toBinaryString(op1) ;
+        return hasilKonversi;
+    }
+ 
+    
 }
